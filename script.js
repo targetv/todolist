cc7236e5883970509d83c330ebe1665ece209a5d
Sean Davison <seandavison1998@hotmail.com>
2021-05-24 15:28:54 +0100

    Added underline

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let state = {
   todos: [],
+  completedtodos: [],
 };
 
 function createElm(tag, attobj) {
@@ -16,13 +17,32 @@ function todoCard(todo) {
   const liEl = document.createElement("li");
   const todoEl = createElm("p", {
     className: "todoBox boxSizing",
-    innerText: `${todo.todoText} \n ${todo.completiondate}`,
+    innerText: `${todo.todoText}`,
   });
+  if (state.completedtodos.find((todoId) => todoId.id === todo.id)) {
+    todoEl.classList.add("underline");
+  }
+
   const todoBtnDel = createElm("button", {
     className: "redButton btn",
     id: "deleteBtn",
     innerText: "-",
   });
+  const completedBtn = createElm("button", {
+    className: "btn redButton",
+    id: "completedBtn",
+    innerText: "tick",
+  });
+  completedBtn.addEventListener("click", function () {
+    const todoId = {
+      id: todo.id,
+    };
+    state.completedtodos.push(todoId);
+    console.log("added to completedtodos", state.completedtodos);
+
+    render();
+  });
+  //   dateTimeTimer(todo.completiondate);
   todoBtnDel.addEventListener("click", function () {
     fetch(`http://localhost:3000/todos/${todo.id}`, {
       method: "DELETE",
@@ -32,9 +52,40 @@ function todoCard(todo) {
       }
     });
   });
-  liEl.append(todoEl, todoBtnDel);
+  liEl.append(todoEl, todoBtnDel, completedBtn);
   return liEl;
 }
+
+// Input: date time string
+// Action: creates timer from date string
+// Output: returns string value
+
+function dateTimeTimer(datetimeString) {
+  let countdowntimer = new Date(datetimeString).getTime();
+
+  // Update the count down every 1 second
+  setInterval(function () {
+    // Get today's date and time
+    let currentTimeDate = new Date().getTime();
+
+    let distance = countdowntimer - currentTimeDate;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    // Output the result in an element with id="demo"
+    const liEl = document.getElementById("timer");
+    const timerEL = createElm("p", {
+      innerText: `${days} d ${hours} hours ${minutes} m `,
+    });
+    liEl.append(timerEL);
+  });
+}
+
 // Action: Create todo li element
 // Output: todo element
 
@@ -50,6 +101,7 @@ function userTodos() {
     const todoEl = todoCard(todo);
     ulEl.append(todoEl);
   }
+
   sectionEl.append(ulEl);
 }
 
@@ -112,6 +164,8 @@ function addUserDataToState() {
 // Action: render todos list
 // Output nothing
 function render() {
+  const sectionEl = document.querySelector("section");
+  sectionEl.innerHTML = "";
   userTodos();
 }
 function init() {
